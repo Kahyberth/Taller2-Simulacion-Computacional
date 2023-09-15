33cab452c3f148c749ac7708dbfe4e58e0a08f5f
Kahyberth <kahyberth.gonzalez@correounivalle.edu.co>
2023-09-15 14:15:58 -0500

    add Taller2

---
+++ generador.py
@@ -0,0 +1,27 @@
+import math
+import numpy as np
+from scipy.stats import chi2
+class Generador:
+    
+    def __init__(self, x0, a, c, m):
+        self.x0 = x0
+        self.a = a
+        self.c = c
+        self.m = m
+    
+    def generadorLinealCongruente(self):
+        xn = self.x0
+        dato = []
+        while True:
+            xn = (self.a * xn + self.c) % self.m
+            rn = xn / self.m
+            if rn in dato:
+                break
+            dato.append(rn)
+            
+        return dato
+
+
+
+
+
---
+++ independencia.py
@@ -0,0 +1,10 @@
+class Independencia:
+    
+    def __init__(self,intervalo):
+        self.intervalo = intervalo
+        
+    def filtrarRango(self, lista):
+        if (self.intervalo != None):
+            pass
+        pass
+        
