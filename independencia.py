33f62a364a87471862c771fb5b6748d4325da615
Kahyberth <kahyberth.gonzalez@correounivalle.edu.co>
2023-09-17 19:51:48 -0500

    add g:kolmogorov_smirnov

--- independencia.py
+++ independencia.py
@@ -13,7 +13,7 @@ class Independencia:
                 ls = 0
                 li = (1 / self.intervalo) + ls
                 save.append([ls, li])
-                for i in range(1, self.intervalo):
+                for i in range(1, self.intervalo + 1):
                     #print([ls, li])
                     ls = li
                     li = (1 / self.intervalo) + ls
@@ -23,7 +23,7 @@ class Independencia:
             ls = 0
             li = (1 / self.intervalo) + ls
             save.append([ls, li])
-            for i in range(1, self.intervalo):
+            for i in range(1, self.intervalo + 1):
                 #print([ls, li])
                 ls = li
                 li = (1 / self.intervalo) + ls
@@ -54,7 +54,83 @@ class Independencia:
         return save
     
     def sumaEstadistico(self):
-        return sum(self.estadistico())
+        suma = sum(self.estadistico())
+        return suma
     
     def chiCuadrado(self):
         return stats.chi2.ppf((1 - 0.05), 9)
+    
+    
+    def frecuenciaObservadaAcumulada(self):
+        save = []
+        for i in self.frecuenciaObservada():
+            save.append(i + sum(save))
+        return save
+    
+    def probabilidadObservadaAcumulada(self):
+        save = []
+        for i in self.frecuenciaObservadaAcumulada():
+            save.append(i / len(self.lista))
+        return save
+    
+    def probabiliadEsperadaAcumulada(self):
+        save = []
+        if self.intervalo is None:
+            self.intervalo =  round(math.sqrt(len(self.lista)))
+            fx = (1 / self.intervalo)
+            for i in range(1,self.intervalo):
+                save.append(fx * (i))
+        elif self.intervalo != None:
+            fx = 1 / self.intervalo
+            for i in range(self.intervalo):
+                save.append(fx * (i + 1))
+        return save
+    
+    
+    def diferenciaAbsoluta(self):
+        save = []
+        for i in range(len(self.probabilidadObservadaAcumulada())-1):
+            save.append(abs(self.probabiliadEsperadaAcumulada()[i] - self.probabilidadObservadaAcumulada()[i]))
+        return save
+        
+    
+    
+    
+    def kolmogorov_smirnov(self):
+    # Llama a los métodos y almacena los resultados
+        self.obtenerIntervalo()
+        frecuencia_observada = self.frecuenciaObservada()
+        frecuencia_acumulada = self.frecuenciaObservadaAcumulada()
+        probabilidad_acumulada = self.probabilidadObservadaAcumulada()
+        probabilidad_esperada_acumulada = self.probabiliadEsperadaAcumulada()
+        diferencia_absoluta = self.diferenciaAbsoluta()
+
+        # Realiza la comparación
+        max_diferencia_absoluta = max(diferencia_absoluta)
+        if max_diferencia_absoluta <= (1.36 / math.sqrt(len(self.lista))):
+            print(max_diferencia_absoluta)
+            print("Se acepta la hipotesis de que los numeros son independientes")
+        else:
+            print(max_diferencia_absoluta)
+            print("Se rechaza la hipotesis de que los numeros son independientes")
+
+        
+    
+    
+    def chi2(self):
+        intervalos = []
+        frecuenciaObservada = []
+        frecuenciaEsperada = []
+        estadistico = []
+        sumaEstadistico = []
+        chiCuadrado = []
+        intervalos.append(self.obtenerIntervalo())
+        frecuenciaObservada.append(self.frecuenciaObservada())
+        frecuenciaEsperada.append(self.frecuenciaEsperada())
+        estadistico.append(self.estadistico())
+        sumaEstadistico.append(self.sumaEstadistico())
+        chiCuadrado.append(self.chiCuadrado())
+        
+        return [intervalos, frecuenciaObservada, frecuenciaEsperada, estadistico, sumaEstadistico, chiCuadrado]
+        
+        
