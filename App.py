d1f92ff1782914ac14f2b9cc0b7367c1d30407ea
Kahyberth <kahyberth.gonzalez@correounivalle.edu.co>
2023-09-19 23:26:27 -0500

    Fix GUI

--- App.py
+++ App.py
@@ -1,124 +1,300 @@
 import flet as ft
+from flet import (Page)
 import numpy as np
 from calcular import Calcular
-
-def main(page: ft.Page):
-    pb = ft.ProgressBar(width=400)
-    def selector(e):
-        datos = []
-        if (e.control.selected_index == 0):
-            layaout.controls.append(
-                ft.Row(
-                    controls=[
-                        ft.Text("Generando Números", style="headlineSmall"),
-                        ft.Column([ ft.Text("Procesando....."), pb]), 
-                    ]
-            ))
-            page.update()  
-            app = Calcular(5, 106, 1283, 6075,["Lineal Congruente", "Chi Cuadrado"])
-            app.generate()  
-            app.independencia()
-            layaout.controls.pop()
-            page.update()     
-            datos = app.datos
-            print(datos[1])
-            
-            
-            
-        elif (e.control.selected_index == 1):            
-            print(datos[1])
-        elif (e.control.selected_index == 2):
-            print("Generar PDF")
-      
-    rail = ft.NavigationRail(
-    selected_index=0,
-    label_type=ft.NavigationRailLabelType.ALL,
-    extended=False,
-    min_width=100,
-    min_extended_width=400,
-    leading=ft.Image("https://www.python.org/static/community_logos/python-logo-master-v3-TM.png", width=100, height=100),
-    group_alignment=-0.9,
-    destinations=[
-        ft.NavigationRailDestination(
-            icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.SETTINGS_OUTLINED, label="Generar"
-        ),
-        ft.NavigationRailDestination(
-            icon_content=ft.Icon(ft.icons.LIST),
-            selected_icon_content=ft.Icon(ft.icons.DONE),
-            label="Independencia",
-        ),
-        ft.NavigationRailDestination(
-            icon_content=ft.Icon(ft.icons.LINE_AXIS_ROUNDED),
-            selected_icon_content=ft.Icon(ft.icons.DONE),
-            label="Uniformidad",
-        ),
-         ft.NavigationRailDestination(
-            label_content=ft.TextField(label="x0", width=80)
-        ),
-         ft.NavigationRailDestination(
-            label_content=ft.TextField(label="a", width=80)
-        ),
-         ft.NavigationRailDestination(
-            label_content=ft.TextField(label="c", width=80)
-        ),
-         ft.NavigationRailDestination(
-             
-            label_content=ft.TextField(label="m", width=80)
-        ),
-        ft.NavigationRailDestination(
-            icon=ft.icons.EXPAND_CIRCLE_DOWN,
-            selected_icon_content=ft.Icon(ft.icons.DONE),
-            label_content=ft.Text("Generar Excel"),
-        ),
+from generarExcel import GenerarExcel
+global calculo
+calculo = 100
+class App:
+    def __init__(self) -> None:
+        pass
+    
+    def main(page: Page):      
+        global resultado 
+        resultado = []
+        cells = []
+        excel = []
+        def loadData(e):
+            if len(resultado) > 0 :
+                print(len(resultado))
+                resultado.pop()
+            if (nav.selected_index == 0):
+                try:
+                    print(tx1.value, tx2.value, tx3.value, tx4.value, [generadores.value, uniformidad.value, independencia.value])
+                    calculo = Calcular(int(tx1.value), int(tx2.value), int(tx3.value), int(tx4.value), [generadores.value, uniformidad.value, independencia.value])
+                    print("Datos cargados correctamente")
+                    page.update()
+                except:
+                    print("Error al cargar los datos")
+            resultado.append(calculo)
         
-    ],
-    on_change=selector,
-    height=800,
-    width=300,
-)
-
-    
-    
-            
-    
-    tabla =  ft.DataTable(
-            columns=[
-                ft.DataColumn(ft.Text("Intervalos")),
-                ft.DataColumn(ft.Text("Frecuencia Observada")),
-                ft.DataColumn(ft.Text("Frecuencia Esperada")),
-                ft.DataColumn(ft.Text("Estadístico")),
-            ],
-            rows= [],
-            width=600,
-            height=800
-        )
-
-    
-    
-
         
-    
-    tableControl =  ft.Ref[ft.Column]()
-    
-    
-    layaout =  ft.Column(
-            controls=[
-                ft.Row(
-                    [
-                        rail,
-                        ft.VerticalDivider(width=1),
-                        ft.Column(ref=tableControl, controls=[tabla], scroll= ft.ScrollMode.ALWAYS, width=600, height=800),
-                        ft.VerticalDivider(width=1),
-                    ],
+        
+        def generarExcel(e):
+            if uniformidad.value == "Chi2":
+                d = {
+                    'Rango': excel[0],
+                    'FO': excel[1],
+                    'FE': excel[2],
+                    'Estadístico': excel[3],
+                    }
+                GenerarExcel(d, "Chi2").generarExcel()
+            elif uniformidad.value == "Kolmogorov":
+                d = {
+                    'Rango': excel[0],
+                    'FO': excel[1],
+                    'FOA': excel[2],
+                    'POA': excel[3],
+                    'PEA': excel[4],
+                    '|POA-PEA|': excel[5],
+                    }
+                gen_excel = GenerarExcel(d, "Kolmogorov")
+                gen_excel.generarExcel()
+            
+       
+        
+        
+        def generar(e):
+            gen_numeros = resultado[0].generate()
+            gen_uniformity = resultado[0].uniformity()
+            
+            if uniformidad.value == "Chi2":
+                for i in range(len(gen_uniformity[0])):
+                    cells.append(
+                        ft.DataRow(
+                            cells=[
+                                ft.DataCell(ft.Text(str(gen_uniformity[0][i]))),
+                                ft.DataCell(ft.Text(str(gen_uniformity[1][i]))),
+                                ft.DataCell(ft.Text(str(gen_uniformity[2][i]))),
+                                ft.DataCell(ft.Text(str(gen_uniformity[3][i]))),
+                            ]
+                        )
+                    )
                     
+                    excel.append(gen_uniformity[0][i])
+                    excel.append(gen_uniformity[1][i])  
+                    excel.append(gen_uniformity[2][i])
+                    excel.append(gen_uniformity[3][i])
+
+                
+        
+                    
+            elif uniformidad.value == "Kolmogorov":
+                for i in range(len(gen_uniformity[0])-1):
+                    cells.append(
+                        ft.DataRow(
+                            cells=[
+                                ft.DataCell(ft.Text(str(gen_uniformity[0][i]))),
+                                ft.DataCell(ft.Text(str(gen_uniformity[1][i]))),
+                                ft.DataCell(ft.Text(str(gen_uniformity[2][i]))),
+                                ft.DataCell(ft.Text(str(gen_uniformity[3][i]))),
+                                ft.DataCell(ft.Text(str(gen_uniformity[4][i]))),
+                                ft.DataCell(ft.Text(str(gen_uniformity[5][i]))),
+                            ]
+                        )
+                    )
+                
+                excel.append(gen_uniformity[0][i])
+                excel.append(gen_uniformity[1][i])
+                excel.append(gen_uniformity[2][i])
+                excel.append(gen_uniformity[3][i])
+                excel.append(gen_uniformity[4][i])
+                excel.append(gen_uniformity[5][i])
+            
+            
+            page.update()
+            print(gen_uniformity)
+        
+        def selector(e):
+            if nav.selected_index == 0:
+                loadData(e)
+            elif nav.selected_index == 1:
+                generar(e)
+            elif nav.selected_index == 2:
+                generarExcel(e)
+        
+        
+        def dropDown(e):
+            if uniformidad.value == "Chi2":
+                try:
+                    if len(panel_table.current.controls) > 0:
+                        panel_table.current.controls.remove(kolmogorovTable)
+                    panel_table.current.controls.append(chi2Table)
+                    page.update()
+                except:
+                    pass
+                
+            elif uniformidad.value == "Kolmogorov":
+                try:
+                    if len(panel_table.current.controls) > 0:
+                        panel_table.current.controls.remove(chi2Table)
+                    panel_table.current.controls.append(kolmogorovTable)
+                    page.update()
+                except:
+                    pass
+                
+                
+        def theme_changed(e):
+            page.theme_mode = (
+                ft.ThemeMode.DARK
+                if page.theme_mode == ft.ThemeMode.LIGHT
+                else ft.ThemeMode.LIGHT
+            )
+            c1.label = (
+                "Light theme" if page.theme_mode == ft.ThemeMode.LIGHT else "Dark theme"
+            )
+            page.update()
+        
+        
+    
+        c1 = ft.Switch(label="Light theme", value = True, on_change=theme_changed)
+        
+        uniformidad = ft.Dropdown(
+            on_change=dropDown,
+            width=200,
+            label="Tipo de Uniformidad",
+            options=[
+                ft.dropdown.Option("Chi2"),
+                ft.dropdown.Option("Kolmogorov")
+            ]
+        )
+        
+        
+        generadores = ft.Dropdown(
+            width=200,
+            label="Tipo de generador",
+            options=[
+                ft.dropdown.Option("Lineal"),
+                ft.dropdown.Option("Estandar Minimo")
+            ]
+        )
+        
+        independencia = ft.Dropdown(
+            width=200,
+            label="Tipo de independencia",
+            options=[
+                ft.dropdown.Option("Corridas"),
+                ft.dropdown.Option("Series"),
+                ft.dropdown.Option("Poker")
+            ]
+        )
+        
+        
+        container = ft.Column(
+            controls=[
+            c1,
+            uniformidad,
+            generadores,
+            independencia
+            ],
+        height= 250
+        )
+        
+        
+        tx1 = ft.TextField(label="X0")
+        tx1.width = 80
+        tx1.height = 35
+        tx2 = ft.TextField(label="a")
+        tx2.width = 80
+        tx2.height = 35
+        tx3 = ft.TextField(label="c")
+        tx3.width = 80
+        tx3.height = 35
+        tx4 = ft.TextField(label="m")
+        tx4.width = 80
+        tx4.height = 35
+        container2 = ft.Column(
+            controls=[
+                tx1,
+                tx2,
+                tx3,
+                tx4  
+            ],
+        )
+        nav = ft.NavigationRail(
+            selected_index=0,
+            label_type=ft.NavigationRailLabelType.ALL,
+            destinations=[
+                ft.NavigationRailDestination(
+                    icon=ft.icons.ACCESSIBILITY, selected_icon=ft.icons.DONE, label="Cargar Datos",
                     
                 ),
+                ft.NavigationRailDestination(
+                    icon=ft.icons.FUNCTIONS,label="Generar"
+                ),
+                ft.NavigationRailDestination(
+                    icon=ft.icons.BOOK, selected_icon=ft.icons.DONE, label="Exportar a Excel"
+                ),
             ],
-            scroll= ft.ScrollMode.ALWAYS,
+            
+            on_change= selector,
+            width= 170,
+            height= 180
         )
-    
-    page.add(
-       layaout
-    )
 
-ft.app(target=main)
+
+        
+
+
+        
+        
+                
+        
+        chi2Table =  ft.DataTable(
+                columns=[
+                    ft.DataColumn(ft.Text("Intervalos")),
+                    ft.DataColumn(ft.Text("Frecuencia Observada")),
+                    ft.DataColumn(ft.Text("Frecuencia Esperada")),
+                    ft.DataColumn(ft.Text("Estadístico")),
+                ],
+                rows= cells,
+                width=1000,
+                height=600
+            )
+        
+        kolmogorovTable =  ft.DataTable(
+                columns=[
+                    ft.DataColumn(ft.Text("Intervalos")),
+                    ft.DataColumn(ft.Text("FO")),
+                    ft.DataColumn(ft.Text("FOA ")),
+                    ft.DataColumn(ft.Text("POA")),
+                    ft.DataColumn(ft.Text("PEA")),
+                    ft.DataColumn(ft.Text("|POA-PEA|")),
+                ],
+                rows= cells,
+                width=1000,
+                height=600
+            )
+
+        
+        
+
+            
+        
+        tableControl =  ft.Ref[ft.Column]()
+        
+        panel = ft.Column(
+            controls=[
+                nav,
+                container,
+                container2,
+            ],
+        )
+        
+        panel_table = ft.Ref[ft.Column]()
+        
+        
+        page.add(ft.Row(
+            controls=[
+                panel,
+                ft.VerticalDivider(width=1),
+                ft.Column(
+                    ref=panel_table,
+                    controls=[],
+                    height=620),
+                ft.VerticalDivider(width=1),
+            ],
+                height=620
+        ))
+            
+    ft.app(target=main)
