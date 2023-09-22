dcc8fbf2de9fac9c49fecd36a042f937001cfe46
Kahyberth <kahyberth.gonzalez@correounivalle.edu.co>
2023-09-22 09:51:02 -0500

    refactor name function

--- generarExcel.py
+++ generarExcel.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import os
+
+
 class GenerarExcel:
     def __init__(self, d, nombre_archivo_excel):
         self.d = d
-        self.nombre_archivo_excel = nombre_archivo_excel
-    
-    def generarExcel(self):
-        nombre_archivo_excel = self.nombre_archivo_excel
-        ruta_completa = os.path.join("Tablas", nombre_archivo_excel + ".xlsx")
+        self.file_name = nombre_archivo_excel
+
+    def generateExcel(self):
+        excel_file = self.file_name
+        path = os.path.join("Results", excel_file + ".xlsx")
         df = pd.DataFrame(data=self.d)
-        df.to_excel(ruta_completa, index=False)
-    
+        df.to_excel(path, index=False)
