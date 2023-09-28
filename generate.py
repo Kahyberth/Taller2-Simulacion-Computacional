f881756b6474e18c8d54f4b3b6f58b74c36339d4
Kahyberth <kahyberth.gonzalez@correounivalle.edu.co>
2023-09-27 20:36:50 -0500

    refactor generator

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 import math
 
-class Generador:
+class Generate:
 
     def __init__(self, x0, a, c, m):
         self.x0 = x0
@@ -8,23 +8,22 @@ class Generador:
         self.c = c
         self.m = m
 
-    def generadorLinealCongruente(self, n=None):
-        xn = self.x0
-        dato = []
-        while n > 0 if n != None else True:
-            xn = (self.a * xn + self.c) % self.m
-            rn = xn / self.m
-            if rn in dato:
-                break
-            dato.append(rn)
+    def congruentLinearGenerator(self, n=None):
+        x = self.x0
+        a = self.a
+        c = self.c
+        m = self.m
+        data = []
+        for i in range(n):
+          x = (a * x + c) % m
+          data.append(x / m)
+        return data
 
-        return dato
-
-    def generadorEstandarMinimo(self, n=None):
+    def minimumStandardGenerator(self):
         q = math.floor(self.m / self.a)
         r = (self.m % self.a)
         xn_1 = self.x0
-        dato = []
+        data = []
         self.m = self.a * q + r
         while True:
             if (self.a * (xn_1 % q) - r * math.floor(xn_1 / q)) >= 0:
@@ -33,15 +32,11 @@ class Generador:
                 modulo = (self.a * (xn_1 % q) - r * math.floor(xn_1 / q) + self.m) % self.m
             xn_1 = modulo
 
-            if xn_1 in dato:
+            if xn_1 in data:
                 break
             else:
-                dato.append(xn_1)
-        return dato
-
-
-
-
+                data.append(xn_1)
+        return data
 
 
 
---
+++ uniformity.py
@@ -0,0 +1,10 @@
+import math
+import numpy
+class Uniformidad:
+    
+    def __init__(self, lista, range = 10):
+        
+        
+
+        
+        
