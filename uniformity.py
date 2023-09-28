5015c16756fcdd4e0eb024f5f900c7592a4d2ca1
Kahyberth <kahyberth.gonzalez@correounivalle.edu.co>
2023-09-27 22:14:05 -0500

    refactor variables

--- uniformity.py
+++ uniformity.py
@@ -1,25 +1,26 @@
 import math
 import numpy
 from prettytable import PrettyTable
-class Uniformity:
-    
-    def __init__(self, data_list, range = 10):
-        self.range = range
-        self.list = data_list
 
+
+class Uniformity:
+
+    def __init__(self, data_list, limit=10):
+        self.range = limit
+        self.list = data_list
 
     def chi_cuadrado(self):
         data_list = self.list
         number_of_classes = self.range
         chi = 0
         x = PrettyTable()
-        x.field_names = ["RANGO", "FO", "FE", "ESTADÍSTICO"]
+        x.field_names = ["RANGO", "fo", "fe", "ESTADÍSTICO"]
         for i in range(number_of_classes):
-            FO = len(list(filter(lambda dato: i / number_of_classes <= dato < (i + 1) / number_of_classes, data_list)))
-            FE = len(data_list) / number_of_classes
-            DI = (math.pow((FO-FE),2)) / FE
-            chi += DI
-            x.add_row([f"[{(i+1)/number_of_classes},{(i+1)/number_of_classes}]",FO,FE,DI])
+            fo = len(list(filter(lambda dato: i / number_of_classes <= dato < (i + 1) / number_of_classes, data_list)))
+            fe = len(data_list) / number_of_classes
+            di = (math.pow((fo - fe), 2)) / fe
+            chi += di
+            x.add_row([f"[{(i + 1) / number_of_classes},{(i + 1) / number_of_classes}]", fo, fe, di])
         print(x)
 
         return chi
@@ -28,19 +29,16 @@ class Uniformity:
         data_list = self.list
         number_of_classes = self.range
         xcal = numpy.array([])
-        FOA = 0
+        foa = 0
         x = PrettyTable()
         x.field_names = ["RANGO", "FO", "FOA", "POA", "PEA", "| PEA - POA |"]
         for i in range(number_of_classes):
-            FO = len(list(filter(lambda dato: i / number_of_classes <= dato < (i + 1) / number_of_classes, data_list)))
-            FOA = FO + FOA
-            POA = FOA / len(data_list)
-            PEA = (i + 1) / number_of_classes
-            xcal = numpy.append(xcal, math.fabs(PEA-POA))
-            x.add_row([f"[{(i + 1) / number_of_classes},{(i + 1) / number_of_classes}]", FO, FOA, POA,PEA,math.fabs(PEA-POA)])
+            fo = len(list(filter(lambda dato: i / number_of_classes <= dato < (i + 1) / number_of_classes, data_list)))
+            foa = fo + foa
+            poa = foa / len(data_list)
+            pea = (i + 1) / number_of_classes
+            xcal = numpy.append(xcal, math.fabs(pea - poa))
+            x.add_row([f"[{i/ number_of_classes},{(i + 1) / number_of_classes}]", fo, foa, poa, pea,
+                       math.fabs(pea - poa)])
 
         print(x)
-
-
-
-
