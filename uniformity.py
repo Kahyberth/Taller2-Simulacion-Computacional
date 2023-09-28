863b2eac9cbbd1ffc2f4c631e257b3e63807c657
Kahyberth <kahyberth.gonzalez@correounivalle.edu.co>
2023-09-27 20:58:26 -0500

    add kolmogorov

--- uniformity.py
+++ uniformity.py
@@ -24,7 +24,22 @@ class Uniformity:
 
         return chi
 
+    def kolmogorov(self):
+        data_list = self.list
+        number_of_classes = self.range
+        xcal = numpy.array([])
+        FOA = 0
+        x = PrettyTable()
+        x.field_names = ["RANGO", "FO", "FOA", "POA", "PEA", "| PEA - POA |"]
+        for i in range(number_of_classes):
+            FO = len(list(filter(lambda dato: i / number_of_classes <= dato < (i + 1) / number_of_classes, data_list)))
+            FOA = FO + FOA
+            POA = FOA / len(data_list)
+            PEA = (i + 1) / number_of_classes
+            xcal = numpy.append(xcal, math.fabs(PEA-POA))
+            x.add_row([f"[{(i + 1) / number_of_classes},{(i + 1) / number_of_classes}]", FO, FOA, POA,PEA,math.fabs(PEA-POA)])
 
+        print(x)
 
 
 
