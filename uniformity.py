4cb3a6374038e9f8b7eec215affab861e6efa1b4
Kahyberth <kahyberth.gonzalez@correounivalle.edu.co>
2023-09-27 20:45:24 -0500

    add chi-cuadrado

--- uniformity.py
+++ uniformity.py
@@ -1,10 +1,31 @@
 import math
 import numpy
-class Uniformidad:
+from prettytable import PrettyTable
+class Uniformity:
     
-    def __init__(self, lista, range = 10):
-        
-        
+    def __init__(self, data_list, range = 10):
+        self.range = range
+        self.list = data_list
 
-        
-        
+
+    def chi_cuadrado(self):
+        data_list = self.list
+        number_of_classes = self.range
+        chi = 0
+        x = PrettyTable()
+        x.field_names = ["RANGO", "FO", "FE", "ESTADÍSTICO"]
+        for i in range(number_of_classes):
+            FO = len(list(filter(lambda dato: i / number_of_classes <= dato < (i + 1) / number_of_classes, data_list)))
+            FE = len(data_list) / number_of_classes
+            DI = (math.pow((FO-FE),2)) / FE
+            chi += DI
+            x.add_row([f"[{(i+1)/number_of_classes},{(i+1)/number_of_classes}]",FO,FE,DI])
+        print(x)
+
+        return chi
+
+
+
+
+
+
