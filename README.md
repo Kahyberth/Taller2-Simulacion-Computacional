c42f3401fa928a538ce9f4b63aa3bb6202ec1c44
Kahyberth <kahyberth.gonzalez@correounivalle.edu.co>
2023-10-02 16:12:28 -0500

    refactor all code

--- README.md
+++
@@ -1,7 +0,0 @@
-# Taller2-Simulacion-Computacional
-
-```
-Kahyberth Stiven Gonzalez Sayas: 2060121-3743
-Juan Camilo Varela Ocoro: 2060166-3743
-Kevin Andres Ordoñez: 2060301-3743
-```
---
+++ src/Uniformity/chicuadrado.js
@@ -0,0 +1,38 @@
+import  Table from 'cli-table3'
+export class Chicuadrado {
+
+    constructor(list) {
+        this.lst = list;
+    }
+
+    chicuadrado() {
+        let table = new Table({
+            head: ['Clase', 'Fo', 'Fe', 'Chi-Cuadrado'],
+            style: {
+                head: [], //disable colors in header cells
+                border: [], //disable colors for the border
+            },
+            colWidths: [30, 30, 30, 30], //set the widths of each column (optional)
+        });
+
+        const data_list = this.lst;
+        const number_of_classes = 10;
+        const grades = number_of_classes - 1;
+        let sumChi = 0;
+        for (let i = 0; i < number_of_classes; i++) {
+            const FO = data_list.filter(data => data >= i / number_of_classes && data < (i + 1) / number_of_classes).length;
+            const FE = data_list.length / number_of_classes;
+            const difference = Math.pow(FO - FE, 2);
+            const chi = difference / FE;
+            sumChi += chi;
+            table.push([`[${(i)/number_of_classes} , ${(i + 1)/number_of_classes}]`,FO, FE, chi])
+        }
+
+        console.log(table.toString())
+
+
+
+    }
+
+
+}
