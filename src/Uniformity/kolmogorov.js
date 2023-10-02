dbb4b98bca011cc6326691feb5bc18842dd1c613
Kahyberth <kahyberth.gonzalez@correounivalle.edu.co>
2023-10-02 16:29:16 -0500

    add Kolomogorov

---
+++ src/Uniformity/kolmogorov.js
@@ -0,0 +1,38 @@
+import Table from "cli-table3";
+export class Kolmogorov {
+
+    constructor(lista) {
+        this.lst = lista;
+    }
+
+    kolmogorov() {
+
+        let table = new Table({
+            head: ['Clase', 'Fo', 'Foa', 'Poa', 'Pea', '|Pea-Poa|'],
+            style: {
+                head: [], //disable colors in header cells
+                border: [], //disable colors for the border
+            },
+            colWidths: [18, 18, 18, 18, 18, 18], //set the widths of each column (optional)
+        });
+
+        const data_list = this.lst;
+        const number_of_classes = 10;
+        const grades = data_list.length;
+        const kolmogorov_data = [];
+        let FOA = 0;
+        for(let i  = 0; i < number_of_classes; i++) {
+            const FO = data_list.filter(data => data >= i / number_of_classes && data < (i + 1) / number_of_classes).length;
+            FOA = FO + FOA;
+            const POA = FOA / data_list.length;
+            const PEA = (i + 1) / number_of_classes;
+            kolmogorov_data.push(Math.abs(PEA - POA));
+            table.push([`[${(i)/number_of_classes} , ${(i + 1)/number_of_classes}]`, FO, FOA, POA,PEA, Math.abs(PEA-POA)]);
+        }
+
+        const dm_calc = kolmogorov_data.reduce((a,b) => Math.max(a,b));
+        console.log(table.toString());
+    }
+
+
+}
